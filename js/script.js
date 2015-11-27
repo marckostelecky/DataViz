5db533c21bbd795f3bba3b44cebfe5b02f850eda
marckostelecky <marc.kostelecky@gmail.com>
2015-11-27 13:09:12 -0600

    jQuizzle in the hizzle
    changed the animations to use jQuery

--- js/script.js
+++ js/script.js
@@ -2,5 +2,26 @@
  * Created by Squeelch on 11/27/2015.
  */
 $(document).ready(function(){
-  console.log("test");
+  onHover();
+  function onHover() {
+    $(".option-icons li").on({
+      mouseenter: function () {
+        var title = $(this).attr('data-title');
+        var _this = $(this);
+        $(this).append('<div class="hoverinfo">' + title + '</div>');
+
+        setTimeout(function () {
+          _this.find(".hoverinfo").addClass("active");
+        }, 50);
+      },
+      mouseleave: function () {
+        var hover = $(this).find(".hoverinfo");
+        hover.removeClass("active");
+        setTimeout(function () {
+          $(hover).remove();
+        }, 200);
+      }
+    });
+  }
 });
+
