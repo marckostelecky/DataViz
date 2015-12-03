a6f4824193c97623962e697d0acce785b28d95fb
Jordan Christiansen <xordspar0@gmail.com>
2015-12-03 13:45:40 -0600

    Adjusted the map some more.

--- js/data-visualization.js
+++ js/data-visualization.js
@@ -1,4 +1,13 @@
 /*
+ * data-visualization.js
+ * Created: 2 December 2015
+ * Authors: Jordan Christiansen
+ *
+ * This is where our data visualization code is. This is where we primarily
+ * work with d3 (d3js.org).
+ */
+
+/*
  * Alternative Fuel Stations
  *
  * This is a map of the locations of alternative fuel stations in the US. The
@@ -33,7 +42,7 @@ function map() {
 		var units = topojson.feature(us, us.objects.units);
 		// Choose a projection for the map data.
 		var projection = d3.geo.albers()
-			.center([0, 38])
+			.center([0, 35])
 			.rotate([100, 0])
 			.parallels([25, 50])
 			.translate([width / 2, height / 2]);
