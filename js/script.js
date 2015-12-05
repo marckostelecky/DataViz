3a25966963bf23132cd9a97342c722927ec80ca1
marckostelecky <marc.kostelecky@gmail.com>
2015-12-05 17:33:07 -0600

    Upload doesn't make a bunch of maps and big cube
    its free real estate

--- js/script.js
+++ js/script.js
@@ -2,8 +2,16 @@
  * Created by Squeelch on 11/27/2015.
  */
 $(document).ready(function(){
+  var uploadVisible = false;
   onHover();
+  /*
+   * onHover()
+   *
+   * Holds the functionality for the UI, activates once an option icon is hovered over.
+   */
   function onHover() {
+    var clicked = false;
+
     $(".option-icons li").on({
       mouseenter: function () {
         var title = $(this).attr('data-title');
@@ -17,43 +25,50 @@ $(document).ready(function(){
 
       click: function() {
         var _this = $(this);
-        if (_this.attr('id') == "create") {
-          var image = '<li class="barChart" data-title="New Bar Graph">' +
-            '<i class="fa fa-bar-chart"></i>' +
-            '</li>' +
-            '<li class="mapChart"  data-title="New Map Graph">' +
-            '<i class="fa fa-map"></i>' +
-            '</li>';
-        } else {
-          var image = '';
-        }
-
-        $(this).append('<ul class="clickinfo">' + image + '</ul>');
-
-        setTimeout(function () {
-          _this.find(".hoverinfo").addClass("clicked");
-          _this.find(".clickinfo").addClass("active");
-        }, 50);
-
-        $(".create li").on({
-          mouseenter: function () {
-            var title = $(this).attr('data-title');
-            var _this = $(this);
-            $(this).append('<div class="hoverinfo2">' + title + '</div>');
-
-            setTimeout(function () {
-              _this.find(".hoverinfo2").addClass("active");
-            }, 50);
-          },
-          mouseleave: function () {
-            var hover = $(this).find(".hoverinfo2");
-            hover.removeClass("active");
-            setTimeout(function () {
-              $(hover).remove();
-            }, 200);
+        if (clicked == false) {
+          if (_this.attr('id') == "create") {
+            var image = '<li class="barChart" data-title="New Bar Graph">' +
+              '<i class="fa fa-bar-chart"></i>' +
+              '</li>' +
+              '<li class="mapChart"  data-title="New Map Graph">' +
+              '<i class="fa fa-map"></i>' +
+              '</li>';
+          } else if (_this.attr('id') == "upload") {
+              var image = '';
+              upload();
+          } else {
+            var image = '';
           }
-        });
-  },
+
+          $(this).append('<ul class="clickinfo">' + image + '</ul>');
+
+          setTimeout(function () {
+            _this.find(".hoverinfo").addClass("clicked");
+            _this.find(".clickinfo").addClass("active");
+          }, 50);
+
+          $(".create li").on({
+            mouseenter: function () {
+              var title = $(this).attr('data-title');
+              var _this = $(this);
+              $(this).append('<div class="hoverinfo2">' + title + '</div>');
+
+              setTimeout(function () {
+                _this.find(".hoverinfo2").addClass("active");
+              }, 50);
+            },
+
+            mouseleave: function () {
+              var hover = $(this).find(".hoverinfo2");
+              hover.removeClass("active");
+              setTimeout(function () {
+                $(hover).remove();
+              }, 200);
+            }
+          });
+        }
+          clicked = true;
+      },
       mouseleave: function () {
         var hover = $(this).find(".hoverinfo");
         var click = $(this).find(".clickinfo");
@@ -62,12 +77,24 @@ $(document).ready(function(){
         setTimeout(function () {
           $(click).remove();
           $(hover).remove();
+          clicked = false;
         }, 200);
       }
     });
-
-
   }
+
+  /*
+   * upload()
+   *
+   * Opens the Fuel Station Map Graph
+   */
+  function upload() {
+    if (uploadVisible == false){
+    uploadVisible = true;
+    var bg = document.getElementById("background");
+    bg.style.visibility = 'hidden';
+    fuel_stations();
+  }}
 });
 
 
