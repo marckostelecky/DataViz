99b9bfcb781f1cd76fc44656d66b15a2ad8f38c9
Jordan Christiansen <xordspar0@gmail.com>
2015-12-07 06:32:09 -0600

    Converted scripts from a mix of tabs and spaces to just spaces

--- js/data-visualization.js
+++ js/data-visualization.js
@@ -19,120 +19,120 @@ var height = 786;
  * http://naciscdn.org/naturalearth/10m/cultural/ne_10m_populated_places.zip
  */
 function map(data) {
-	// Delete the old visualization, if there is one.
-	d3.select("main svg").remove();
+  // Delete the old visualization, if there is one.
+  d3.select("main svg").remove();
 
-	// Create the svg element where we will create the map.
-	var map_svg = d3.select("main").append("svg")
-		.attr("width", width)
-		.attr("height", height)
-		.attr("class", "map");
+  // Create the svg element where we will create the map.
+  var map_svg = d3.select("main").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "map");
 
-	// Choose a projection for the map data.
-	var projection = d3.geo.albers()
-		.center([0, 35])
-		.rotate([100, 0])
-		.parallels([25, 50])
-		.translate([width / 2, height / 2]);
+  // Choose a projection for the map data.
+  var projection = d3.geo.albers()
+    .center([0, 35])
+    .rotate([100, 0])
+    .parallels([25, 50])
+    .translate([width / 2, height / 2]);
 
-	// First, make the background map.
-	d3.json("../data/us.json", function(error, us) {
-		if (error) return console.error(error);
-		console.log(us);
+  // First, make the background map.
+  d3.json("../data/us.json", function(error, us) {
+    if (error) return console.error(error);
+    console.log(us);
 
-		// Generate the svg path from the data.
-		var projection_path = d3.geo.path().projection(projection);
-		projection_path.pointRadius(1);
+    // Generate the svg path from the data.
+    var projection_path = d3.geo.path().projection(projection);
+    projection_path.pointRadius(1);
 
-		// Draw the states.
-		map_svg.append("path")
-			.datum(topojson.feature(us, us.objects.units))
-			.attr("d", projection_path)
-			.attr("id", "map");
-		// Draw the state boundaries.
-		map_svg.append("path")
-			.datum(topojson.mesh(us, us.objects.units, function(a, b) { return a !== b; }))
-			.attr("d", projection_path)
-			.attr("class", "state-boundary");
-		// Add points for major cities.
-		map_svg.append("path")
-			.datum(topojson.feature(us, us.objects.places))
-			.attr("d", projection_path)
-			.attr("class", "place");
-	});
+    // Draw the states.
+    map_svg.append("path")
+      .datum(topojson.feature(us, us.objects.units))
+      .attr("d", projection_path)
+      .attr("id", "map");
+    // Draw the state boundaries.
+    map_svg.append("path")
+      .datum(topojson.mesh(us, us.objects.units, function(a, b) { return a !== b; }))
+      .attr("d", projection_path)
+      .attr("class", "state-boundary");
+    // Add points for major cities.
+    map_svg.append("path")
+      .datum(topojson.feature(us, us.objects.places))
+      .attr("d", projection_path)
+      .attr("class", "place");
+  });
 
-	// Add the user's data to the map.
-	if (data != null) {
-		d3.json(data, function(error, data) {
-			if (error) return console.error(error);
-			console.log(data);
+  // Add the user's data to the map.
+  if (data != null) {
+    d3.json(data, function(error, data) {
+      if (error) return console.error(error);
+      console.log(data);
 
-			// Generate the svg path from the data.
-			var projection_path = d3.geo.path().projection(projection);
-			projection_path.pointRadius(1);
+      // Generate the svg path from the data.
+      var projection_path = d3.geo.path().projection(projection);
+      projection_path.pointRadius(1);
 
-			// Add the data to the map.
-			map_svg = d3.select(".map");
-			map_svg.append("path")
-				.datum(topojson.feature(data,
-							data.objects.dataPoints))
-				.attr("d", projection_path)
-				.attr("class", "data-point");
-		});
-	}
+      // Add the data to the map.
+      map_svg = d3.select(".map");
+      map_svg.append("path")
+        .datum(topojson.feature(data,
+              data.objects.dataPoints))
+        .attr("d", projection_path)
+        .attr("class", "data-point");
+    });
+  }
 }
 
 /*
  * Make a bar graph.
  */
 function bar(barData) {
-	// Delete the old visualization, if there is one.
-	d3.select("main svg").remove();
+  // Delete the old visualization, if there is one.
+  d3.select("main svg").remove();
 
-	// Create the svg element where we will create the bar graph.
-	var barSvg = d3.select("main").append("svg")
-		.attr("width", width)
-		.attr("height", height)
-		.style("padding", "100px 100px 100px 100px")
-		.attr("class", "barChart");
+  // Create the svg element where we will create the bar graph.
+  var barSvg = d3.select("main").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("padding", "100px 100px 100px 100px")
+    .attr("class", "barChart");
 
-	// Set the dimentions of the bar chart.
-	var chartWidth = 800;
-	var chartHeight = 600;
+  // Set the dimentions of the bar chart.
+  var chartWidth = 800;
+  var chartHeight = 600;
 
-	var x = d3.scale.ordinal()
-		.rangeRoundBands([0, chartWidth], .1);
-	var y = d3.scale.linear()
-		.range([chartHeight/2, 0]);
+  var x = d3.scale.ordinal()
+    .rangeRoundBands([0, chartWidth], .1);
+  var y = d3.scale.linear()
+    .range([chartHeight/2, 0]);
 
-	var xAxis = d3.svg.axis()
-		.scale(x)
-		.orient("bottom");
-	var yAxis = d3.svg.axis()
-		.scale(y)
-		.orient("left");
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
 
-	// Load the data that goes in the chart.
-	d3.json(barData, function(error, barData) {
-		if (error) return console.error(error);
-		console.log(barData);
+  // Load the data that goes in the chart.
+  d3.json(barData, function(error, barData) {
+    if (error) return console.error(error);
+    console.log(barData);
 
-		x.domain(barData.data.map(function(d) { return d.category; }));
-		y.domain([0, d3.max(barData.data, function(d) { return d.value; } )]);
-		var barWidth = chartWidth / barData.data.length;
+    x.domain(barData.data.map(function(d) { return d.category; }));
+    y.domain([0, d3.max(barData.data, function(d) { return d.value; } )]);
+    var barWidth = chartWidth / barData.data.length;
 
-		barSvg.append("g")
-			.attr("class", "xAxis")
-			.attr("transform", "translate(0, " + chartHeight + ")")
-			.call(xAxis);
+    barSvg.append("g")
+      .attr("class", "xAxis")
+      .attr("transform", "translate(0, " + chartHeight + ")")
+      .call(xAxis);
 
-		barSvg.selectAll(".bar")
-			.data(barData.data)
-			.enter().append("rect")
-			.attr("class", "bar")
-			.attr("x", function(d) { return x(d.category); })
-			.attr("y", function(d) { return y(d.value); })
-			.attr("height", function(d) { return chartHeight - y(d.value); })
-			.attr("width", x.rangeBand());
-	});
+    barSvg.selectAll(".bar")
+      .data(barData.data)
+      .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.category); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return chartHeight - y(d.value); })
+      .attr("width", x.rangeBand());
+  });
 }
--- js/script.js
+++ js/script.js
@@ -2,313 +2,313 @@
  * Created by Squeelch on 11/27/2015.
  */
 $(document).ready(function(){
-  var uploadVisible = false;
-  var bg = document.getElementById("background");
-  var currentGraph = null;
-  var elementNum = 0;
-  var baseChart;
-  onHover();
-  /*
-   * onHover()
-   *
-   * Holds the functionality for the UI, activates once an option icon is hovered over.
-   */
-  function onHover() {
-    var clicked = false;
+	var uploadVisible = false;
+	var bg = document.getElementById("background");
+	var currentGraph = null;
+	var elementNum = 0;
+	var baseChart;
+	onHover();
+	/*
+	 * onHover()
+	 *
+	 * Holds the functionality for the UI, activates once an option icon is hovered over.
+	 */
+	function onHover() {
+		var clicked = false;
 
-    /* Interaction functionality when using the buttons on the navbar
-     *  mouseenter appends a hoverinfo css
-     *  click is what happens when you click
-     *  mouseleave will remove the appended elements
-     * */
-    $(".option-icons li").on({
-      mouseenter: function () {
-        var title = $(this).attr('data-title');
-        var _this = $(this);
-        $(this).append('<div class="hoverinfo">' + title + '</div>');
+		/* Interaction functionality when using the buttons on the navbar
+		 *  mouseenter appends a hoverinfo css
+		 *  click is what happens when you click
+		 *  mouseleave will remove the appended elements
+		 * */
+		$(".option-icons li").on({
+			mouseenter: function () {
+				var title = $(this).attr('data-title');
+				var _this = $(this);
+				$(this).append('<div class="hoverinfo">' + title + '</div>');
 
-        setTimeout(function () {
-          _this.find(".hoverinfo").addClass("active");
-        }, 50);
-      },
-      /* Click functionality for original Option Icons
-       *
-       */
-      click: function() {
-        var _this = $(this);
-        if (clicked == false) {
-          if (_this.attr('id') == "create") {
-            var image = '<li class="barChart" data-title="New Bar Graph" id="bar">' +
-              '<i class="fa fa-bar-chart"></i>' +
-              '</li>' +
-              '<li class="mapChart"  data-title="New Map Graph" id="map">' +
-              '<i class="fa fa-map"></i>' +
-              '</li>';
-          } else if (_this.attr('id') == "upload") {
-            var image = '<li class="barChart" data-title="Upload Bar Graph" id="bar">' +
-              '<i class="fa fa-bar-chart"></i>' +
-              '</li>' +
-              '<li class="mapChart"  data-title="Upload Map Graph" id="map">' +
-              '<i class="fa fa-map"></i>' +
-              '</li>';
-          }
-          else if (_this.attr('id') == "add") {
-            if (currentGraph == 'bar') {
-            }
-            else if (currentGraph == 'map') {
-            }
-            else {
-              alert('You must have an open data set or be creating a data set in order to add an element to it!');
-            }
-          } else {
+				setTimeout(function () {
+					_this.find(".hoverinfo").addClass("active");
+				}, 50);
+			},
+			/* Click functionality for original Option Icons
+			 *
+			 */
+			click: function() {
+				var _this = $(this);
+				if (clicked == false) {
+					if (_this.attr('id') == "create") {
+						var image = '<li class="barChart" data-title="New Bar Graph" id="bar">' +
+							'<i class="fa fa-bar-chart"></i>' +
+							'</li>' +
+							'<li class="mapChart"  data-title="New Map Graph" id="map">' +
+							'<i class="fa fa-map"></i>' +
+							'</li>';
+					} else if (_this.attr('id') == "upload") {
+						var image = '<li class="barChart" data-title="Upload Bar Graph" id="bar">' +
+							'<i class="fa fa-bar-chart"></i>' +
+							'</li>' +
+							'<li class="mapChart"  data-title="Upload Map Graph" id="map">' +
+							'<i class="fa fa-map"></i>' +
+							'</li>';
+					}
+					else if (_this.attr('id') == "add") {
+						if (currentGraph == 'bar') {
+						}
+						else if (currentGraph == 'map') {
+						}
+						else {
+							alert('You must have an open data set or be creating a data set in order to add an element to it!');
+						}
+					} else {
 
-          }
+					}
 
-          $(this).append('<ul class="clickinfo">' + image + '</ul>');
+					$(this).append('<ul class="clickinfo">' + image + '</ul>');
 
-          setTimeout(function () {
-            _this.find(".hoverinfo").addClass("clicked");
-            _this.find(".clickinfo").addClass("active");
-          }, 50);
+					setTimeout(function () {
+						_this.find(".hoverinfo").addClass("clicked");
+						_this.find(".clickinfo").addClass("active");
+					}, 50);
 
-          /*
-           * Code for the Create button
-           */
-          $(".create li").on({
-            mouseenter: function () {
-              var title = $(this).attr('data-title');
-              var _this = $(this);
-              $(this).append('<div class="hoverinfo2">' + title + '</div>');
+					/*
+					 * Code for the Create button
+					 */
+					$(".create li").on({
+						mouseenter: function () {
+							var title = $(this).attr('data-title');
+							var _this = $(this);
+							$(this).append('<div class="hoverinfo2">' + title + '</div>');
 
-              setTimeout(function () {
-                _this.find(".hoverinfo2").addClass("active");
-              }, 50);
-            },
+							setTimeout(function () {
+								_this.find(".hoverinfo2").addClass("active");
+							}, 50);
+						},
 
-            click: function () {
-              var title = $(this).attr('data-title');
-              if (title == "New Bar Graph") {
-                if ($('.elementAdd').length == 0) {
-                  barCreate();
-                  newElement();
-                }
-                else {
-                  removeDialog();
-                  barCreate();
-                  newElement();
-                }
-            }
-              else if (title == "New Map Graph") {
-                if ($('.elementAdd').length == 0) {
-                  mapCreate();
-                  newElement();
-                }
-                else {
-                  removeDialog();
-                  mapCreate();
-                  newElement();
-                }
-              }
-            },
+						click: function () {
+							var title = $(this).attr('data-title');
+							if (title == "New Bar Graph") {
+								if ($('.elementAdd').length == 0) {
+									barCreate();
+									newElement();
+								}
+								else {
+									removeDialog();
+									barCreate();
+									newElement();
+								}
+							}
+							else if (title == "New Map Graph") {
+								if ($('.elementAdd').length == 0) {
+									mapCreate();
+									newElement();
+								}
+								else {
+									removeDialog();
+									mapCreate();
+									newElement();
+								}
+							}
+						},
 
-            mouseleave: function () {
-              var hover = $(this).find(".hoverinfo2");
-              hover.removeClass("active");
-              setTimeout(function () {
-                $(hover).remove();
-              }, 200);
-            }
-          });
+						mouseleave: function () {
+							var hover = $(this).find(".hoverinfo2");
+							hover.removeClass("active");
+							setTimeout(function () {
+								$(hover).remove();
+							}, 200);
+						}
+					});
 
-          /*
-           * Code for the Create button
-           */
-          $(".upload li").on({
-            mouseenter: function () {
-              var title = $(this).attr('data-title');
-              var _this = $(this);
-              $(this).append('<div class="hoverinfo2">' + title + '</div>');
+					/*
+					 * Code for the Create button
+					 */
+					$(".upload li").on({
+						mouseenter: function () {
+							var title = $(this).attr('data-title');
+							var _this = $(this);
+							$(this).append('<div class="hoverinfo2">' + title + '</div>');
 
-              setTimeout(function () {
-                _this.find(".hoverinfo2").addClass("active");
-              }, 50);
-            },
+							setTimeout(function () {
+								_this.find(".hoverinfo2").addClass("active");
+							}, 50);
+						},
 
 
-        click: function () {
-          var title = $(this).attr('data-title');
-          if (title == "Upload Bar Graph") {
-            uploadBar();
-          }
-          else if (title == "Upload Map Graph") {
-            bg.style.visibility = 'hidden';
-            uploadMap();
-          }
-        },
+						click: function () {
+							var title = $(this).attr('data-title');
+							if (title == "Upload Bar Graph") {
+								uploadBar();
+							}
+							else if (title == "Upload Map Graph") {
+								bg.style.visibility = 'hidden';
+								uploadMap();
+							}
+						},
 
-            mouseleave: function () {
-              var hover = $(this).find(".hoverinfo2");
-              hover.removeClass("active");
-              setTimeout(function () {
-                $(hover).remove();
-              }, 200);
-            }
-          });
-        }
-        clicked = true;
-      },
-      // mouseleave for option icons
-      mouseleave: function () {
-        var hover = $(this).find(".hoverinfo");
-        var click = $(this).find(".clickinfo");
-        hover.removeClass("active");
-        $(click).remove();
-        setTimeout(function () {
-          $(click).remove();
-          $(hover).remove();
-          clicked = false;
-        }, 200);
-      }
-    });
-  }
+						mouseleave: function () {
+							var hover = $(this).find(".hoverinfo2");
+							hover.removeClass("active");
+							setTimeout(function () {
+								$(hover).remove();
+							}, 200);
+						}
+					});
+				}
+				clicked = true;
+			},
+			// mouseleave for option icons
+			mouseleave: function () {
+				var hover = $(this).find(".hoverinfo");
+				var click = $(this).find(".clickinfo");
+				hover.removeClass("active");
+				$(click).remove();
+				setTimeout(function () {
+					$(click).remove();
+					$(hover).remove();
+					clicked = false;
+				}, 200);
+			}
+		});
+	}
 
-  /*
-   * Upload Data Sets
-   *
-   * This would allow the user to upload the dataset of his or her choosing to
-   * the web application, but in our prototype it imports predefined demo
-   * datasets.
-   *
-   * The code of the demos is in js/data-visualization-demos.js.
-   */
+	/*
+	 * Upload Data Sets
+	 *
+	 * This would allow the user to upload the dataset of his or her choosing to
+	 * the web application, but in our prototype it imports predefined demo
+	 * datasets.
+	 *
+	 * The code of the demos is in js/data-visualization-demos.js.
+	 */
 
-  // Uploads the fuel station map graph
-  function uploadMap() {
-      bg.style.visibility = 'hidden';
-      fuel_stations();
-    }
+	// Uploads the fuel station map graph
+	function uploadMap() {
+		bg.style.visibility = 'hidden';
+		fuel_stations();
+	}
 
-  // Uploads the bar graph data
-  function uploadBar() {
-      bg.style.visibility = 'hidden';
-      ndsu_enrollment();
-  }
+	// Uploads the bar graph data
+	function uploadBar() {
+		bg.style.visibility = 'hidden';
+		ndsu_enrollment();
+	}
 
-  function barCreate() {
-    currentGraph = 'bar';
-    baseCase = {"data": [
-      {"category": "0", "value": 0},
-      {"category": "1", "value": 0},
-      {"category": "2", "value": 0}
-    ]
-    };
+	function barCreate() {
+		currentGraph = 'bar';
+		baseCase = {"data": [
+			{"category": "0", "value": 0},
+			{"category": "1", "value": 0},
+			{"category": "2", "value": 0}
+		]
+		};
 
-    $('.dialogBox').append('<div id="dialogImage">' + '<div class="addButton"><i class="fa fa-plus"></i></div>' +
-    '<div class="deleteButton"><i class="fa fa-minus"></i></div>' +
-    '<i class="fa fa-bar-chart"></i>' + 'New Bar Graph' +
-    '<div class="close-button"><i class="fa fa-times"></i></div>' +
-    '</div>' + '<div class="elementAdd"></div>' +
-    '<div id="dialogBottom">Create!</div>');
+		$('.dialogBox').append('<div id="dialogImage">' + '<div class="addButton"><i class="fa fa-plus"></i></div>' +
+				'<div class="deleteButton"><i class="fa fa-minus"></i></div>' +
+				'<i class="fa fa-bar-chart"></i>' + 'New Bar Graph' +
+				'<div class="close-button"><i class="fa fa-times"></i></div>' +
+				'</div>' + '<div class="elementAdd"></div>' +
+				'<div id="dialogBottom">Create!</div>');
 
-    $('.addButton').on({
-      click: function () {
-        newElement();
-      }
-    });
+		$('.addButton').on({
+			click: function () {
+				newElement();
+			}
+		});
 
-    $('.deleteButton').on({
-      click: function () {
-        removeElement();
-      }
-    });
+		$('.deleteButton').on({
+			click: function () {
+				removeElement();
+			}
+		});
 
-    $('.close-button').on({
-      click: function () {
-        removeDialog();
-      }
-    });
+		$('.close-button').on({
+			click: function () {
+				removeDialog();
+			}
+		});
 
-  }
-  function mapCreate() {
-    currentGraph = 'map';
-    baseCase = {"data": [
-      {"category": "0", "value": 0},
-      {"category": "1", "value": 0},
-      {"category": "2", "value": 0}
-    ]
-    };
+	}
+	function mapCreate() {
+		currentGraph = 'map';
+		baseCase = {"data": [
+			{"category": "0", "value": 0},
+			{"category": "1", "value": 0},
+			{"category": "2", "value": 0}
+		]
+		};
 
-    $('.dialogBox').append('<div id="dialogImage">' +
-    '<div class="addButton"><i class="fa fa-plus"></i></div>' +
-    '<div class="deleteButton"><i class="fa fa-minus"></i></div>' +
-    '<i class="fa fa-map"></i>' + 'New Map Graph' +
-    '<div class="close-button"><i class="fa fa-times"></i></div>' +
-    '</div>' + '<div class="elementAdd"></div>' +
-    '<div id="dialogBottom">Create!</div>');
+		$('.dialogBox').append('<div id="dialogImage">' +
+				'<div class="addButton"><i class="fa fa-plus"></i></div>' +
+				'<div class="deleteButton"><i class="fa fa-minus"></i></div>' +
+				'<i class="fa fa-map"></i>' + 'New Map Graph' +
+				'<div class="close-button"><i class="fa fa-times"></i></div>' +
+				'</div>' + '<div class="elementAdd"></div>' +
+				'<div id="dialogBottom">Create!</div>');
 
-    $('.addButton').on({
-      click: function () {
-        newElement();
-      }
-    });
+		$('.addButton').on({
+			click: function () {
+				newElement();
+			}
+		});
 
-    $('.deleteButton').on({
-      click: function () {
-        removeElement();
-      }
-    });
+		$('.deleteButton').on({
+			click: function () {
+				removeElement();
+			}
+		});
 
-    $('.close-button').on({
-      click: function () {
-        removeDialog();
-      }
-    });
+		$('.close-button').on({
+			click: function () {
+				removeDialog();
+			}
+		});
 
-    //map();
-    //bg.style.visibility = 'hidden';
-  }
+		//map();
+		//bg.style.visibility = 'hidden';
+	}
 
-  function newElement() {
-    baseCase.data.push({"category": "3", "value": 0});
-    refreshElement();
-  }
+	function newElement() {
+		baseCase.data.push({"category": "3", "value": 0});
+		refreshElement();
+	}
 
-  function removeElement() {
-    baseCase.data.pop();
-    refreshElement();
-  }
-  function refreshElement() {
-    //$('.elementAdd').append('<div class="element">' + elementNum + '</div>')
-   // $('.element').attr("id", elementNum);
+	function removeElement() {
+		baseCase.data.pop();
+		refreshElement();
+	}
+	function refreshElement() {
+		//$('.elementAdd').append('<div class="element">' + elementNum + '</div>')
+		// $('.element').attr("id", elementNum);
 
-    d3.selectAll('.element').remove();
+		d3.selectAll('.element').remove();
 
-    d3.select('.elementAdd').selectAll('.element')
-      .data(baseCase.data)
-      .enter().append('div')
-      .attr('id', function(d, i) { return i;})
-      .attr('class', 'element');
+		d3.select('.elementAdd').selectAll('.element')
+			.data(baseCase.data)
+			.enter().append('div')
+			.attr('id', function(d, i) { return i;})
+			.attr('class', 'element');
 
-    if (currentGraph == 'bar') {
-      $('.element').each(function (i, e) {
-        $(this).append('<span id="title">Title</span>' + '<input type="text" class="keyboard" placeholder="' + baseCase.data[i].category + '"/>' + '<span id="title">Value</span>' + '<input type="text" class="keyboard" placeholder="' + baseCase.data[i].value + '"/>');
-      });
-    } else if (currentGraph == "map") {
-      $('.element').each(function (i, e) {
-        $(this).append('<span id="title">Long</span>' + '<input type="text" class="keyboard" placeholder="' + baseCase.data[i].category + '"/>' + '<span id="title">Lat</span>' + '<input type="text" class="keyboard" placeholder="' + baseCase.data[i].value + '"/>');
-      });
-    }
+		if (currentGraph == 'bar') {
+			$('.element').each(function (i, e) {
+				$(this).append('<span id="title">Title</span>' + '<input type="text" class="keyboard" placeholder="' + baseCase.data[i].category + '"/>' + '<span id="title">Value</span>' + '<input type="text" class="keyboard" placeholder="' + baseCase.data[i].value + '"/>');
+			});
+		} else if (currentGraph == "map") {
+			$('.element').each(function (i, e) {
+				$(this).append('<span id="title">Long</span>' + '<input type="text" class="keyboard" placeholder="' + baseCase.data[i].category + '"/>' + '<span id="title">Lat</span>' + '<input type="text" class="keyboard" placeholder="' + baseCase.data[i].value + '"/>');
+			});
+		}
 
-    makeKeyboard();
-    elementNum++;
-  }
+		makeKeyboard();
+		elementNum++;
+	}
 
-  function removeDialog() {
-    $('#dialogImage').remove();
-    $('.elementAdd').remove();
-    $('#dialogBottom').remove();
-    elementNum = 0;
-  }
+	function removeDialog() {
+		$('#dialogImage').remove();
+		$('.elementAdd').remove();
+		$('#dialogBottom').remove();
+		elementNum = 0;
+	}
 });
 
 
